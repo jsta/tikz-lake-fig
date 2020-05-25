08ac4e2fffb8734f812afacaeaee83f8633156c6
jsta <stachel2@msu.edu>
2020-05-25 10:58:09 -0400

    create fields even when not validating

--- ctan.R
+++ ctan.R
@@ -3,20 +3,21 @@ args <- commandArgs(trailingOnly = TRUE)
 
 library(httr)
 
+# ---- validate payload ----
+fields_validate <- list(
+  author = "Joseph Stachelek",
+  pkg = "tikz-lake-fig",
+  version = "1.0.0",
+  email = "stachel2@msu.edu",
+  uploader = "Joseph Stachelek",
+  license = "lppl1.3c",
+  summary = "A variety of customizable lake diagrams",
+  repository = "https://github.com/jsta/tikz-lake-fig", 
+  bugtracker = "https://github.com/jsta/tikz-lake-fig/issues",
+  support = "https://github.com/jsta/tikz-lake-fig/issues",
+  file = upload_file("tikz-lake-fig.zip"))
+
 if ("--validate" %in% args) {
-  # ---- validate payload ----
-  fields_validate <- list(
-    author = "Joseph Stachelek",
-    pkg = "tikz-lake-fig",
-    # version = "1.0.0",
-    email = "stachel2@msu.edu",
-    uploader = "Joseph Stachelek",
-    license = "lppl1.3c",
-    summary = "A variety of customizable lake diagrams",
-    repository = "https://github.com/jsta/tikz-lake-fig", 
-    bugtracker = "https://github.com/jsta/tikz-lake-fig/issues",
-    support = "https://github.com/jsta/tikz-lake-fig/issues",
-    file = upload_file("tikz-lake-fig.zip"))
   validate <- httr::POST("https://www.ctan.org/submit/validate", 
                          body = fields_validate)
   if (length(content(validate, "parsed")) > 0) {
@@ -30,10 +31,12 @@ if ("--validate" %in% args) {
 # ---- build ----
 if ("--build" %in% args) {
   setwd("../")
+  file.copy("tikz-lake-fig/README_ctan.md", "README.md")
   unlink("tikz-lake-fig/tikz-lake-fig.zip"); unlink("tikz-lake-fig.zip")
-  zip("tikz-lake-fig.zip", paste0("tikz-lake-fig/",
-                                  c("README.md", "tikz-lake-fig.sty", 
-                                    "tikz-lake-fig-doc.tex", "tikz-lake-fig-doc.pdf")))
+  zip("tikz-lake-fig.zip", c("README.md", paste0("tikz-lake-fig/",
+                                  c("tikz-lake-fig.sty", 
+                                    "tikz-lake-fig-doc.tex", "tikz-lake-fig-doc.pdf"))))
+  unlink("README.md")
   file.rename("tikz-lake-fig.zip", "tikz-lake-fig/tikz-lake-fig.zip")
   setwd("tikz-lake-fig")
 }
